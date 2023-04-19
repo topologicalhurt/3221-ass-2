ff67581398d4f9af4b6041dd67565bb683b05408
topologicalhurt <csin0659@uni.sydney.edu.au>
2023-04-20 01:05:51 +1000

    Added files to track

---
+++ conf.py
@@ -0,0 +1,5 @@
+class Conf:
+    """Creates namespace dynamically from kwargs"""
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            setattr(self, k, v)
--- utils.py
+++ utils.py
@@ -1,6 +1,31 @@
 import argparse
+import json
+import socket
+from contextlib import closing
+
+from conf import Conf
 
 class Utils:
+
+    @staticmethod
+    def __read_conf() -> Conf:
+        with open('net_conf.json', 'r') as f:
+            data = json.load(f)
+        return Conf(**data)
+
+    NET_CONF = __read_conf()
+
+    @staticmethod
+    def thread_spawner(func):
+        def wrapper(*args, **kwargs):
+            threads = []
+            func(threads, *args, **kwargs)
+            for t in threads:
+                t.start()
+            for t in threads:
+                t.join()
+
+        return wrapper
 
     @staticmethod
     def verify_flag(arg: str) -> bool:
@@ -17,3 +42,24 @@ class Utils:
                 return True
             case '0':
                 return False
+
+    @staticmethod
+    def port_no_valid(arg: str) -> int:
+        if not all(c.isdigit() for c in arg):
+            raise argparse.ArgumentTypeError('Must be an integer')
+        arg = int(arg)
+
+        with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+            if not sock.connect_ex((Utils.NET_CONF.host, arg)) != 0:
+                raise argparse.ArgumentTypeError('The port must not already be open!')
+
+        if __debug__:
+            if arg > 0xffff or arg < 6000:
+                raise argparse.ArgumentTypeError('Must be in range [6000, 0xffff]')
+        else:
+            if arg != Utils.NET_CONF.port:
+                raise argparse.ArgumentTypeError('Port number must be 6000 in production mode')
+
+        return arg
+
+
